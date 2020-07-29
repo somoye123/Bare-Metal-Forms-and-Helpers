74d6aa7f88a42479856186fc2a60eb6749117227
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-29 13:28:55 -0700

    Update your routes and controller to handle editing an existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to new_user_path
+
+  end
 
   private
 
