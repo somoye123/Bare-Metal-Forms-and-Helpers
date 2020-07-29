b1c0c03f4fd4f20cfd7c9d8ef385a100df2350d5
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-29 14:06:44 -0700

    fixed linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
-  
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
@@ -23,7 +22,6 @@ class UsersController < ApplicationController
     @user.update(user_params)
 
     redirect_to new_user_path
-
   end
 
   private
